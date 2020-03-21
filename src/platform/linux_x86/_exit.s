e0fb47aa3a90a25dee71fb56f682ce356414d867
Johan Norberg <norberg_johan@hotmail.com>
2020-03-21 19:21:18 +0100

    Support linux x86 cdecl

---
+++ src/platform/linux_x86/_exit.s
@@ -0,0 +1,8 @@
+.global _exit
+
+.text
+_exit:
+    # Exit code is first argument on the stack
+    movl 4(%esp), %ebx # ebx should contain the return code
+    movl $1, %eax      # System call number 1: exit()
+    int  $0x80         # Invoke system call
