0dee4a621eb690ec9e6880332b714dfe774c4dca
Johan Norberg <norberg_johan@hotmail.com>
2020-03-29 17:27:36 +0200

    Support arguments to main on Linux x86

--- test/src/test_main_args.c
+++ test/src/test_main_args.c
@@ -1,10 +1,15 @@
 #include "stdbool.h"
 
 int main2(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         return 1;
     }
-    const bool success = argv[1][0] == 'h' && argv[1][1] == 'e' &&
-                         argv[1][2] == 'j' && argv[1][3] == '\0';
-    return success ? 0 : 1;
+    const bool arg1ok = argv[1][0] == 'h' && argv[1][1] == 'e' &&
+                        argv[1][2] == 'j' && argv[1][3] == '\0';
+    if (!arg1ok) {
+        return 1;
+    }
+    const bool arg2ok = argv[2][0] == 's' && argv[2][1] == 'a' &&
+                        argv[2][2] == 'n' && argv[2][3] == '\0';
+    return arg2ok ? 0 : 1;
 }
