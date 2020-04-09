fbb055dad5b4320c70752412025e5d56b4359fa9
Johan Norberg <norberg_johan@hotmail.com>
2020-04-08 22:35:35 -0500

    Support args on Windows

---
+++ src/platform/win_x86_64/start.c
@@ -0,0 +1,27 @@
+#include "stddef.h"
+#include "stdlib.h"
+
+#include <windows.h>
+
+int _stdlib2_start();
+
+enum { kMaxArgs = 64 };
+
+// Adapted from http://stackoverflow.com/a/13281447/4612829
+static int CommandLineToArgvA_simple(LPSTR command_line, char *argv[]) {
+    int argc = 0;
+    char *next_arg = strtok(command_line, " ");
+    while (next_arg && argc < kMaxArgs - 1) {
+        argv[argc++] = next_arg;
+        next_arg = strtok(NULL, " ");
+    }
+    argv[argc] = NULL;
+    return argc;
+}
+
+void _start() {
+    char *argv[kMaxArgs];
+    int argc = CommandLineToArgvA_simple(GetCommandLine(), argv);
+
+    exit(_stdlib2_start(argc, argv));
+}
--- src/start.c
+++
@@ -1,8 +0,0 @@
-#include "stdlib.h"
-#include "stddef.h"
-
-int _stdlib2_start();
-
-void _start() {
-    exit(_stdlib2_start(0, NULL));
-}
